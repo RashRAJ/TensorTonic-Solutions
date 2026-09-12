9136cb67d8bddead71cd2126679cbfb89611c719
Rasheedat Atinuke Jamiu <56790407+RashRAJ@users.noreply.github.com>
2026-09-12 12:12:17 +0100

    Solve gelu

---
+++ study-plans/cuda-basics/gelu/gelu.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void gelu_kernel(const float* input, float* output, int N) {
+    // Write code here
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    float k = 1.0f / sqrt(2.0f);
+    if (i < N) output[i] = 0.5 * ( input[i] * (1 + erff(input[i] * k)));
+    
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    int threads = 256;
+    dim3 blocks((N + 255) / 256);
+    gelu_kernel<<<blocks, threads>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
